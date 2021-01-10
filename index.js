645991acee34567c4b254c8071712a96e9a8f55b
natanielfcy <a31730027@sina.com>
2021-01-10 08:01:10 +0800

    make it better

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ app.get("/", (req,res) => {
     });
 })
 app.use("/posts", postRoutes);
-const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(process.env.CONNECTION_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(()=> {
-    app.listen(PORT, () => {
-        console.log(`server is running on port: ${PORT}`);
+    app.listen(process.env.PORT, () => {
+        console.log(`server is running on port: ${process.env.PORT}`);
     })
 }).catch(error => {
     console.log(error.message);
